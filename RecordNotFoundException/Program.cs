def2029f64ef7473bdc2cbc6d8f89e2af48d583c
Arda ONUR <arda.onur@hotmail.com>
2021-01-31 09:30:59 +0200

    Add project files.

---
+++ RecordNotFoundException/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordNotFoundException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exception Example
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFound exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void Find()
+        {
+            //Sample customers from db
+            List<string> customers = new List<string> { "Arda", "Cumhur", "Nazire" };
+
+            if (!customers.Contains("Arzu"))
+            {
+                throw new RecordNotFound("Kayıt bulunamadı");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt bulundu");
+            }
+        }
+    }
+}
